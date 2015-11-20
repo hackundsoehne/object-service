ec4975518b1193734185d3f29c4306829e51ecf6
Marcel Hollerbach <marcel-hollerbach@t-online.de>
2015-11-20 13:30:08 +0100

    crowdplatform: added basic interface

---
+++ src/main/java/edu/ipd/kit/crowdcontrol/proto/crowdplatform/CrowdPlatformFactory.java
@@ -0,0 +1,10 @@
+package edu.ipd.kit.crowdcontrol.proto.crowdplatform;
+
+/**
+ * Created by marcel on 20.11.15.
+ */
+public class CrowdPlatformFactory {
+    public CrowdPlatform getCrowdplatform(String name) {
+        return null;
+    }
+}
