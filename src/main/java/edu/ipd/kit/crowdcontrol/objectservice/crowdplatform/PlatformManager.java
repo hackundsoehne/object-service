02e5bd279032dbb7291e07b393cdef7e2562c976
Marcel Hollerbach <marcel-hollerbach@t-online.de>
2016-01-02 15:54:23 +0100

    WIP

--- src/main/java/edu/ipd/kit/crowdcontrol/objectservice/crowdplatform/PlatformManager.java
+++ src/main/java/edu/ipd/kit/crowdcontrol/objectservice/crowdplatform/PlatformManager.java
@@ -15,6 +15,7 @@ public class PlatformManager {
     private final Payment servicPayment;
 
     public PlatformManager(List<Platform> crowdPlatforms) {
+        //TODO the two interfaces can be passed in the contstructor to allow external fallback handing
         dbAssertion = new DataBaseAssertion();
         servicPayment = new ServicePayment();
         platforms = crowdPlatforms.stream()
@@ -22,17 +23,37 @@ public class PlatformManager {
     }
 
     /**
+     * Will get you the instance of a platform interface of a platform, this instance is the same for all calls
+     * @param name The name of the instance to use
+     * @return The optional crowd platform instance
+     */
+    public Optional<Platform> getPlatform(String name) {
+        return Optional.ofNullable(platforms.get(name));
+    }
+    /**
      * Will get you the instance of a publish interface of a platform, this instance is the same for all calls
      * @param name The name of the instance to use
      * @return The optional crowd platform instance
      */
     public Optional<Publish> getPlatformPublish(String name) {
-        return Optional.ofNullable(platforms.get(name)).map(platform -> platform.getPublish());
+        return getPlatform(name).map(platform -> platform.getPublish());
     }
+
+    /**
+     * Will get you the instance of a assertion interface of a platform, this instance is the same for all calls
+     * @param name The name of the instance to use
+     * @return The optional crowd platform instance
+     */
     public Optional<Assertion> getPlatformAssertion(String name) {
-        return Optional.ofNullable(platforms.get(name)).map(platform -> platform.getAssertion().orElse(dbAssertion));
+        return getPlatform(name).map(platform -> platform.getAssertion().orElse(dbAssertion));
     }
+
+    /**
+     * Will get you the instance of a payment interface of a platform, this instance is the same for all calls
+     * @param name The name of the instance to use
+     * @return The optional crowd platform instance
+     */
     public Optional<Payment> getPlatformPayment(String name) {
-        return Optional.ofNullable(platforms.get(name)).map(platform -> platform.getPayment().orElse(servicPayment));
+        return getPlatform(name).map(platform -> platform.getPayment().orElse(servicPayment));
     }
 }
