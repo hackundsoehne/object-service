7fba2198dd8d025a98ba3ad4880a6e5ed1d14f76
Marcel Hollerbach <marcel-hollerbach@t-online.de>
2016-01-15 12:02:25 +0100

    Revert "Publish crowdplatform package"
    This reverts commit 74877a93d43e5910ab2836177a7c0cf0972725a8.

--- src/main/java/edu/ipd/kit/crowdcontrol/objectservice/crowdplatform/ServicePayment.java
+++
@@ -1,8 +0,0 @@
-package edu.ipd.kit.crowdcontrol.objectservice.crowdplatform;
-
-/**
- * NOP class should handle the case when a platform does not support payment
- * Created by marcel on 02.01.16.
- */
-public class ServicePayment implements Payment {
-}
