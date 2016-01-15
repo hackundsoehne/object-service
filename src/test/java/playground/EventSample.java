300a6d3733bb8ff64e318d9795182bd453a8bc01
Marcel Hollerbach <marcel-hollerbach@t-online.de>
2016-01-15 12:14:28 +0100

    Change Event Sample

--- src/test/java/playground/EventSample.java
+++ src/test/java/playground/EventSample.java
@@ -6,11 +6,12 @@ import edu.kit.ipd.crowdcontrol.objectservice.event.EventObservable;
 /**
  * Created by marcel on 17.12.15.
  */
-public class Test {
+public class EventSample {
     public static void main(String[] args) {
         EventObservable<ChangeEvent<String>> ob = new EventObservable<>();
 
-        ob.getObservable().subscribe(stringChangeEvent -> System.out.println("Change "+stringChangeEvent.getOld()+" to "+stringChangeEvent.getNeww()));
+        ob.getObservable().subscribe(stringChangeEvent ->
+                System.out.println("Change "+stringChangeEvent.getOld()+" to "+stringChangeEvent.getNeww()));
 
 
         ob.emit(new ChangeEvent<>("Mensch", "Leander"));
