e0b1a780e04802239b59f2be277784c853b6fdf4
Marcel Hollerbach <marcel-hollerbach@t-online.de>
2016-01-15 11:54:02 +0100

    event: add new events

--- src/main/java/edu/kit/ipd/crowdcontrol/objectservice/event/EventManager.java
+++ src/main/java/edu/kit/ipd/crowdcontrol/objectservice/event/EventManager.java
@@ -1,16 +1,24 @@
 package edu.kit.ipd.crowdcontrol.objectservice.event;
 
-import edu.ipd.kit.crowdcontrol.objectservice.database.model.tables.Answers;
-import edu.ipd.kit.crowdcontrol.objectservice.database.model.tables.Experiment;
-import edu.ipd.kit.crowdcontrol.objectservice.database.model.tables.Ratings;
+import edu.kit.ipd.crowdcontrol.objectservice.proto.*;
 
 /**
  * Created by marcel on 02.01.16.
  */
 public class EventManager {
-    public static final EventObservable<Experiment> NEW_EXPERIMENT = new EventObservable<>();
-    public static final EventObservable<ChangeEvent<Experiment>> CHANGE_EXPERIMENT = new EventObservable<>();
-    public static final EventObservable<Experiment> DEL_EXPERIMENT = new EventObservable<>();
-    public static final EventObservable<Answers> NEW_ANSWER = new EventObservable<>();
-    public static final EventObservable<Ratings> NEW_RATINGS = new EventObservable<>();
+    public static final EventObservable<Experiment> EXPERIMENT_CREATE = new EventObservable<>();
+    public static final EventObservable<ChangeEvent<Experiment>> EXPERIMENT_CHANGE = new EventObservable<>();
+    public static final EventObservable<Experiment> EXPERIMENT_DELETE = new EventObservable<>();
+    public static final EventObservable<Answer> ANSWER_CREATE = new EventObservable<>();
+    public static final EventObservable<Rating> RATINGS_CREATE = new EventObservable<>();
+    public static final EventObservable<Worker> WORKER_CREATE = new EventObservable<>();
+    public static final EventObservable<Worker> WORKER_DELETE = new EventObservable<>();
+    public static final EventObservable<Template> TEMPLATE_CREATE = new EventObservable<>();
+    public static final EventObservable<ChangeEvent<Template>> TEMPLATE_UPDATE = new EventObservable<>();
+    public static final EventObservable<Template> TEMPLATE_DELETE = new EventObservable<>();
+    public static final EventObservable<Population> POPULATION_CREATE = new EventObservable<>();
+    public static final EventObservable<Population> POPULATION_UPDATE = new EventObservable<>();
+    public static final EventObservable<Notification> NOTIFICATION_CREATE = new EventObservable<>();
+    public static final EventObservable<ChangeEvent<Notification>> NOTIFICATION_UPDATE = new EventObservable<>();
+    public static final EventObservable<Notification> NOTIFICATION_DELETE = new EventObservable<>();
 }
