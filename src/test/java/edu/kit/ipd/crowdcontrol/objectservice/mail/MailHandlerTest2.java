15625da5de0ba0f924ecc003cd35f2ee8c16ba6c
Felix Rittler <felix.rittler@web.de>
2016-01-15 23:05:23 +0100

    Minor changes at gmail test

--- src/test/java/edu/kit/ipd/crowdcontrol/objectservice/mail/MailHandlerTest2.java
+++ src/test/java/edu/kit/ipd/crowdcontrol/objectservice/mail/MailHandlerTest2.java
@@ -24,20 +24,20 @@ public class MailHandlerTest2 {
         Properties props = new Properties();
         props.put("sender", "pseipd@gmail.com");
         props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "465");
-        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.port", "587");
+        //props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
         props.put("mail.smtp.auth", "true");
-        //props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.tls", "true");
         props.put("mail.smtp.ssl.checkserveridentity", "true");
         props.put("mail.store.protocol", "imap");
         props.put("mail.imap.host", "imap.gmail.com");
         props.put("mail.imap.port", "993");
-        //props.put("mail.imap.ssl", "true");
-        //props.put("mail.imap.ssl.enable", "true");
-       // props.put("mail.imap.auth.mechanisms", "XOAUTH2");
-       // java.security.Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
-        //props.put("mail.imap.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.imap.ssl", "true");
+        props.put("mail.imap.ssl.enable", "true");
+        // props.put("mail.imap.auth.mechanisms", "XOAUTH2");
+        java.security.Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
+        props.put("mail.imap.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
         Authenticator auth = new Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
