f568857223ae335e78df9a453a6de33c66bf5d4e
lucaskrauss <luc.k@online.de>
2016-01-15 21:25:38 +0000

    Added operations to AnswerRatingOperations

--- src/main/java/edu/kit/ipd/crowdcontrol/objectservice/database/operations/AnswerRatingOperations.java
+++ src/main/java/edu/kit/ipd/crowdcontrol/objectservice/database/operations/AnswerRatingOperations.java
@@ -35,19 +35,42 @@ public class AnswerRatingOperations extends AbstractOperations {
         this.workerCalibrationOperations = workerCalibrationOperations;
     }
 
-    public  Map<AnswerRecord,List<RatingRecord>> getAnswersWithRatings(int experimentID){
-        return null;
-    }
 
+      /**
+     * Gets all ratings of a specified answer
+     * @param answerRecord answer, whose ratings are requested
+     * @return list of ratings of a specified answer
+     */
     public Result<RatingRecord> getRatingsOfAnswer(AnswerRecord answerRecord){
         return null;
     }
 
+
+    /**
+     * Get all answers of the experiment specified by given ID
+     * @param expID specifying the experiment
+     * @return list of all answers of a experiment
+     */
     public Result<AnswerRecord> getAnswersOfExperiment(int expID){
         return null;
     }
 
+
+    /**
+     * Sets quality ratings to a set of ratings
+     * @param map of ratings and matching qualities
+     */
     public void setQualityToRatings(Map<RatingRecord,Integer> map){}
+
+    /**
+     * Returns all ratings of given answer, which have a quality rating above passed threshold
+     * @param answerRecord answer, whose good ratings (specified by given threshold) are returned
+     * @param threshold of type int, which specifies good ratings
+     * @return list of all ratings of given answer with a quality rating equal or greater than given threshold
+     */
+    public Result<RatingRecord> getGoodRatingsOfAnswer(AnswerRecord answerRecord, int threshold){
+        return null;
+    }
 
     /**
      * inserts a new answer into the DB
