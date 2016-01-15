09dc4df71bc9ad4b228f094a29591d42a18550d1
lucaskrauss <luc.k@online.de>
2016-01-15 12:09:58 +0000

    Initing quality-branch

---
+++ src/main/java/edu/kit/ipd/crowdcontrol/objectservice/quality/AnswerQualityIdentification.java
@@ -0,0 +1,7 @@
+package edu.kit.ipd.crowdcontrol.objectservice.quality;
+
+/**
+ * Created by lucas on 15.01.16.
+ */
+public abstract class AnswerQualityIdentification {
+}
---
+++ src/main/java/edu/kit/ipd/crowdcontrol/objectservice/quality/QualityIdentificator.java
@@ -0,0 +1,23 @@
+package edu.kit.ipd.crowdcontrol.objectservice.quality;
+
+import rx.Observer;
+
+/**
+ * Created by lucas on 15.01.16.
+ */
+public class QualityIdentificator implements Observer<T> {
+    @Override
+    public void onCompleted() {
+
+    }
+
+    @Override
+    public void onError(Throwable e) {
+
+    }
+
+    @Override
+    public void onNext(T t) {
+
+    }
+}
