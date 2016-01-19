b4ae87f3b935745c01c33173098859cc58620776
Felix Rittler <felix.rittler@web.de>
2016-01-19 17:37:19 +0100

    updated MoneyTransferManager

--- src/main/java/edu/kit/ipd/crowdcontrol/objectservice/moneytransfer/MoneyTransferManager.java
+++ src/main/java/edu/kit/ipd/crowdcontrol/objectservice/moneytransfer/MoneyTransferManager.java
@@ -1,5 +1,7 @@
 package edu.kit.ipd.crowdcontrol.objectservice.moneytransfer;
 
+import edu.kit.ipd.crowdcontrol.objectservice.database.DatabaseManager;
+
 /**
  * Describes a Manager for money transfers. The Manager can log payments and pay off.
  * @author Felix Rittler
@@ -14,7 +16,9 @@ public class MoneyTransferManager {
      * @param workerID the id of the worker, who gets the money
      * @param amount the amount of money in ct
      */
-    public void logMoneyTransfer(int workerID, int amount) {}
+    public void logMoneyTransfer(int workerID, int amount) {
+        DatabaseManager manager = new DatabaseManager()
+    }
 
     /**
      * Pays all workers depending on their logged money transfers.
