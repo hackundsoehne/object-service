9efbdb94d8a646dd1d7e3962c8e11e6cab67f9b5
Felix Rittler <felix.rittler@web.de>
2016-01-20 01:54:14 +0100

    better constructor of MoneyTransferHandler

--- src/main/java/edu/kit/ipd/crowdcontrol/objectservice/moneytransfer/MoneyTransferManager.java
+++ src/main/java/edu/kit/ipd/crowdcontrol/objectservice/moneytransfer/MoneyTransferManager.java
@@ -1,6 +1,11 @@
 package edu.kit.ipd.crowdcontrol.objectservice.moneytransfer;
 
-import edu.kit.ipd.crowdcontrol.objectservice.database.DatabaseManager;
+import edu.kit.ipd.crowdcontrol.objectservice.mail.MailHandler;
+
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.MessagingException;
 
 /**
  * Describes a Manager for money transfers. The Manager can log payments and pay off.
@@ -8,17 +13,18 @@ import edu.kit.ipd.crowdcontrol.objectservice.database.DatabaseManager;
  */
 public class MoneyTransferManager {
 
+    MailHandler mailHandler;
 
-    public MoneyTransferManager(){}
+    public MoneyTransferManager(MailHandler mailHandler) throws MessagingException{
+        this.mailHandler = mailHandler;
+    }
 
     /**
      * Logs a new money transfer and saves it.
      * @param workerID the id of the worker, who gets the money
      * @param amount the amount of money in ct
      */
-    public void logMoneyTransfer(int workerID, int amount) {
-        DatabaseManager manager = new DatabaseManager()
-    }
+    public void logMoneyTransfer(int workerID, int amount) {    }
 
     /**
      * Pays all workers depending on their logged money transfers.
