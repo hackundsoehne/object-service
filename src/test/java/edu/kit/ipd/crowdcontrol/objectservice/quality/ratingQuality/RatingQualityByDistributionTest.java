905a3b030aacdc814ed89dfd6ea547f2cc300627
hectorPascal <sly94@live.de>
2016-01-25 22:41:10 +0100

    More in test

--- src/test/java/edu/kit/ipd/crowdcontrol/objectservice/quality/ratingQuality/RatingQualityByDistributionTest.java
+++ src/test/java/edu/kit/ipd/crowdcontrol/objectservice/quality/ratingQuality/RatingQualityByDistributionTest.java
@@ -20,7 +20,7 @@ public class RatingQualityByDistributionTest {
     RatingQualityByDistribution identifier;
     List<RatingRecord> ratings ;
     final static int maxQuality = 9;
-    final static int minQuqlity = 0;
+    final static int minQuality = 0;
 
 
     @Before
@@ -35,15 +35,15 @@ public class RatingQualityByDistributionTest {
     }
 
    @Test (expected = IllegalArgumentException.class)
-    public void throwException(){
+    public void throwExceptionInvalidRating(){
        ratings.add(new RatingRecord(-1,0,0,null,-1,0,-1));
-       identifier.identifyRatingQuality(ratings, maxQuality,minQuqlity);
+       identifier.identifyRatingQuality(ratings, maxQuality, minQuality);
 
     }
 
 
     @Test
-    public void optimalRating(){
+    public void optimalRatingEqualsMostChosenRating(){
         ratings.add(new RatingRecord(1,0,0,null,0,0,0));
         ratings.add(new RatingRecord(1,0,0,null,9,0,0));
         ratings.add(new RatingRecord(1,0,0,null,3,0,0));
@@ -53,8 +53,47 @@ public class RatingQualityByDistributionTest {
         ratings.add(ratingRecord);
 
 
-        Map<RatingRecord,Integer> map = identifier.identifyRatingQuality(ratings,maxQuality,minQuqlity);
+        Map<RatingRecord,Integer> map = identifier.identifyRatingQuality(ratings,maxQuality, minQuality);
 
         assertEquals((int)map.get(ratingRecord),maxQuality);
+    }
+
+
+    @Test
+    public void optimalRatingEqualsAverageOfMostChosenRatings(){
+        RatingRecord r1a = new RatingRecord(1,0,0,null,0,0,0);
+        RatingRecord r1b = new RatingRecord(1,0,0,null,0,0,0);
+
+        RatingRecord r2a = new RatingRecord(1,0,0,null,8,0,0);
+        RatingRecord r2b = new RatingRecord(1,0,0,null,8,0,0);
+
+        RatingRecord r3 = new RatingRecord(1,0,0,null,4,0,0);
+
+
+        ratings.add(r1a);
+        ratings.add(r1b);
+        ratings.add(r2a);
+        ratings.add(r2b);
+        ratings.add(r3);
+
+        Map<RatingRecord,Integer> map = identifier.identifyRatingQuality(ratings,maxQuality, minQuality);
+
+
+        assertEquals((int)map.get(r1a),5);
+
+        assertEquals((int)map.get(r2a),5);
+
+        assertEquals((int)map.get(r3),maxQuality);
+
+
+
+
+    }
+
+
+    @Test (expected = IllegalArgumentException.class)
+    public void throwExceptionMissingRatings(){
+        identifier.identifyRatingQuality(ratings,maxQuality, minQuality);
+
     }
 }
