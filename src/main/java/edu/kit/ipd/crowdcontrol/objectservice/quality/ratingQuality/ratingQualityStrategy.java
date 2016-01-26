281eaba21e09b5958827e6a01a8b2e1787708370
hectorPascal <luc.k@online.de>
2016-01-26 17:02:04 +0100

    Added missing ratingQualityStrategy and more test

---
+++ src/main/java/edu/kit/ipd/crowdcontrol/objectservice/quality/ratingQuality/ratingQualityStrategy.java
@@ -0,0 +1,24 @@
+package edu.kit.ipd.crowdcontrol.objectservice.quality.ratingQuality;
+
+import edu.kit.ipd.crowdcontrol.objectservice.database.model.tables.records.RatingRecord;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by lucaskrauss at 19.01.2016
+ *
+ * The RatingQualityStrategy-interface is part of a strategy-design-pattern.
+ * Classes which implement this interface calculate the quality of a set of ratings.
+ * These classes are passed a set of ratings of an answer.
+ */
+public interface RatingQualityStrategy {
+
+      /**
+      * Identifies the quality of a set of ratings
+      * @param ratings list of RatingRecords whose quality will be rated
+      * @return Map of RatingRecords and their assigned quality-value
+      * @throws IllegalArgumentException If ratings have a illegal value
+      */
+     Map<RatingRecord, Integer> identifyRatingQuality(List<RatingRecord> ratings, int maximumQuality, int minimumQuality) throws IllegalArgumentException;
+}
---
+++ src/test/java/edu/kit/ipd/crowdcontrol/objectservice/quality/answerQuality/AnswerQualityByRatingsTest.java
@@ -0,0 +1,56 @@
+package edu.kit.ipd.crowdcontrol.objectservice.quality.answerQuality;
+
+import edu.kit.ipd.crowdcontrol.objectservice.database.model.tables.records.AnswerRecord;
+import edu.kit.ipd.crowdcontrol.objectservice.database.model.tables.records.RatingRecord;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by lucaskrauss at 25.01.2016
+ */
+public class AnswerQualityByRatingsTest {
+
+
+    final static int maxQuality = 9;
+    final static int minQuality = 0;
+    AnswerQualityByRatings identifier;
+    List<RatingRecord> ratings;
+    AnswerRecord answer;
+
+    @Before
+    public void setUp() throws Exception {
+
+        identifier = new AnswerQualityByRatings();
+        answer = new AnswerRecord(1,0,null,null,0,-1);
+        ratings = new LinkedList<>();
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        identifier = null;
+        answer = null;
+        ratings = null;
+
+    }
+
+
+    @Test (expected = IllegalArgumentException.class)
+    public void throwExceptionIllegalRating(){
+        ratings.add(new RatingRecord(0,0,0,null,-1,0,0));
+        identifier.identifyAnswerQuality(answer,ratings, maxQuality,minQuality);
+    }
+
+    @Test
+    public void answerQualityIsAverageOfRatings(){
+
+
+
+
+
+    }
+}
