ab0d12fa78d03a05ff1851e5077df9b89a338060
Leander Kurscheidt <Leander.Kurscheidt@gmx.de>
2016-01-29 20:47:49 +0100

    database: added javadoc

--- src/main/java/edu/kit/ipd/crowdcontrol/objectservice/database/transformers/AbstractTransformer.java
+++ src/main/java/edu/kit/ipd/crowdcontrol/objectservice/database/transformers/AbstractTransformer.java
@@ -30,17 +30,38 @@ public abstract class AbstractTransformer {
         return x;
     }
 
+    /**
+     * constructs a builder-helper used when your data to merge into may be null.
+     * @param x the builder
+     * @param <X> the type of the builder
+     * @return the builder wrapped in a helper
+     */
     protected static <X extends GeneratedMessage.Builder<X>> BuilderHelper<X> builder(X x) {
         return new BuilderHelper<>(x);
     }
 
+    /**
+     * helps constructing a message if your data may be null
+     * @param <B> the type of the builder
+     */
     protected static class BuilderHelper <B extends GeneratedMessage.Builder<B>> {
         private B b;
 
+        /**
+         * creates a new instance of BuilderHelper
+         * @param b the builder to use
+         */
         public BuilderHelper(B b) {
             this.b = b;
         }
 
+        /**
+         * executes the merge-function if the passed x is not null
+         * @param x the data to set
+         * @param merge the merge-function
+         * @param <X> the type of the data
+         * @return this, used for chaining
+         */
         public <X> BuilderHelper<B> set(X x, BiFunction<B, X, B> merge) {
             if (x != null) {
                 b = merge.apply(b, x);
@@ -48,6 +69,10 @@ public abstract class AbstractTransformer {
             return this;
         }
 
+        /**
+         * returns the resulting builder
+         * @return the resulting builder
+         */
         public B getBuilder() {
             return b;
         }
