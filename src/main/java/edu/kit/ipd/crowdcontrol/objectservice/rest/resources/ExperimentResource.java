ebe307998cf8fbcbe142f90f1593b94ac57c050f
Leander Kurscheidt <Leander.Kurscheidt@gmx.de>
2016-01-29 20:43:28 +0100

    rest: improved code-quality

--- src/main/java/edu/kit/ipd/crowdcontrol/objectservice/rest/resources/ExperimentResource.java
+++ src/main/java/edu/kit/ipd/crowdcontrol/objectservice/rest/resources/ExperimentResource.java
@@ -221,103 +221,130 @@ public class ExperimentResource {
 
             if (size > 1)
                 throw new IllegalStateException("if you change the state nothing else can be changed");
-
-            //validate the only two possible changes
-            if (!experiment.getState().equals(Experiment.State.PUBLISHED)
-                    && !experiment.getState().equals(Experiment.State.CREATIVE_STOPPED))
-                throw new IllegalArgumentException("Only "+ Experiment.State.PUBLISHED.name()+
-                        " and " +Experiment.State.CREATIVE_STOPPED.name()+
-                        " is allowed as state change");
-
-            //validate its draft -> published
-            if (!(experiment.getState().equals(Experiment.State.PUBLISHED) && old.getState().equals(Experiment.State.DRAFT))) {
-                throw new IllegalArgumentException("Publish is only allowed for DRAFT experiments");
-            }
-
-            //validate its published -> creative_stopped
-            if (!(experiment.getState().equals(Experiment.State.CREATIVE_STOPPED) && old.getState().equals(Experiment.State.PUBLISHED))) {
-                throw new IllegalArgumentException("Creative stop is only allowed for published experiments");
-            }
-
-            //check that there are enough datas for publish
-            if (experiment.getState().equals(Experiment.State.PUBLISHED)
-                    && experimentOperations.verifyExperimentForPublishing(id)) {
-                throw new IllegalStateException("experiment lacks information needed for publishing");
-            }
-
-            //TODO check if there is a chance to creative stopp is not possible? like no creative answers?
-
-            resulting = fetchExperiment(id);
-            resulting = resulting.toBuilder().setState(experiment.getState()).build();
+            resulting = updateExperimentState(id, experiment, old);
         } else {
-            if (!old.getState().equals(Experiment.State.DRAFT)) {
-                throw new IllegalStateException("When an experiment is running, only the state is allowed to be changed.");
-            }
-
-            ExperimentRecord experimentRecord = ExperimentTransformer.mergeProto(original, experiment);
-            experimentRecord.setIdExperiment(id);
-
-            //update tags if they were updated
-            List<TagRecord> tags = TagConstraintTransformer.getTags(experiment);
-            if (!tags.isEmpty()) {
-                tagConstraintsOperations.deleteAllTags(id);
-                tags.forEach(tagConstraintsOperations::insertTag);
-            }
-
-            //update constraints if they were changed
-            List<ConstraintRecord> constraints = TagConstraintTransformer.getConstraints(experiment);
-            if (!constraints.isEmpty()) {
-                tagConstraintsOperations.deleteAllConstraint(id);
-                constraints.forEach(tagConstraintsOperations::insertConstraint);
-            }
-
-            //update calibration records from the experiment
-            List<ExperimentsCalibrationRecord> records = convertToCalibrationRecords(experiment);
-            if (!records.isEmpty()) {
-                calibrationOperations.deleteAllExperimentCalibration(id);
-                records.forEach(calibrationOperations::insertExperimentCalibration);
-            }
-
-            if (!Objects.equals(old.getAlgorithmTaskChooser().getName(), experimentRecord.getAlgorithmTaskChooser())) {
-                algorithmsOperations.deleteChosenTaskChooserParams(id);
-            }
-
-            experiment.getAlgorithmTaskChooser().getParametersList().forEach(param -> {
-                algorithmsOperations.storeTaskChooserParam(id, param.getId(), param.getValue());
-            });
-
-            if (!Objects.equals(old.getAlgorithmQualityAnswer().getName(), experimentRecord.getAlgorithmQualityAnswer())) {
-                algorithmsOperations.deleteChosenAnswerQualityParams(id);
-            }
-
-            experiment.getAlgorithmQualityAnswer().getParametersList().forEach(param -> {
-                algorithmsOperations.storeAnswerQualityParam(id, param.getId(), param.getValue());
-            });
-
-            if (!Objects.equals(old.getAlgorithmQualityRating().getName(), experimentRecord.getAlgorithmQualityRating())) {
-                algorithmsOperations.deleteChosenRatingQualityParams(id);
-            }
-
-            experiment.getAlgorithmQualityRating().getParametersList().forEach(param -> {
-                algorithmsOperations.storeRatingQualityParam(id, param.getId(), param.getValue());
-            });
-
-            if (!Objects.equals(old.getTemplateId(), experimentRecord.getTemplate())) {
-                experimentOperations.deleteRatingOptions(id);
-            }
-
-            if (!experiment.getRatingOptionsList().isEmpty()) {
-                experimentOperations.storeRatingOptions(experiment.getRatingOptionsList(), id);
-            }
-
-            //update the experiment itself
-            experimentOperations.updateExperiment(experimentRecord);
-
-            resulting = fetchExperiment(id);
+            resulting = updateExperimentInfo(id, experiment, old, original);
         }
 
         EventManager.EXPERIMENT_CHANGE.emit(new ChangeEvent<>(old, resulting));
 
+        return resulting;
+    }
+
+    /**
+     * updates the information about an experiment
+     * @param id the id of the experiment
+     * @param experiment the experiment with the new data
+     * @param old the old experiment in the protobuf-format
+     * @param oldRecord the old experiment as the database-record
+     * @return the resulting experiment
+     */
+    private Experiment updateExperimentInfo(int id, Experiment experiment, Experiment old, ExperimentRecord oldRecord) {
+        Experiment resulting;
+        if (!old.getState().equals(Experiment.State.DRAFT)) {
+            throw new IllegalStateException("When an experiment is running, only the state is allowed to be changed.");
+        }
+
+        ExperimentRecord experimentRecord = ExperimentTransformer.mergeProto(oldRecord, experiment);
+        experimentRecord.setIdExperiment(id);
+
+        //update tags if they were updated
+        List<TagRecord> tags = TagConstraintTransformer.getTags(experiment);
+        if (!tags.isEmpty()) {
+            tagConstraintsOperations.deleteAllTags(id);
+            tags.forEach(tagConstraintsOperations::insertTag);
+        }
+
+        //update constraints if they were changed
+        List<ConstraintRecord> constraints = TagConstraintTransformer.getConstraints(experiment);
+        if (!constraints.isEmpty()) {
+            tagConstraintsOperations.deleteAllConstraint(id);
+            constraints.forEach(tagConstraintsOperations::insertConstraint);
+        }
+
+        //update calibration records from the experiment
+        List<ExperimentsCalibrationRecord> records = convertToCalibrationRecords(experiment);
+        if (!records.isEmpty()) {
+            calibrationOperations.deleteAllExperimentCalibration(id);
+            records.forEach(calibrationOperations::insertExperimentCalibration);
+        }
+
+        if (!Objects.equals(old.getAlgorithmTaskChooser().getName(), experimentRecord.getAlgorithmTaskChooser())) {
+            algorithmsOperations.deleteChosenTaskChooserParams(id);
+        }
+
+        experiment.getAlgorithmTaskChooser().getParametersList().forEach(param -> {
+            algorithmsOperations.storeTaskChooserParam(id, param.getId(), param.getValue());
+        });
+
+        if (!Objects.equals(old.getAlgorithmQualityAnswer().getName(), experimentRecord.getAlgorithmQualityAnswer())) {
+            algorithmsOperations.deleteChosenAnswerQualityParams(id);
+        }
+
+        experiment.getAlgorithmQualityAnswer().getParametersList().forEach(param -> {
+            algorithmsOperations.storeAnswerQualityParam(id, param.getId(), param.getValue());
+        });
+
+        if (!Objects.equals(old.getAlgorithmQualityRating().getName(), experimentRecord.getAlgorithmQualityRating())) {
+            algorithmsOperations.deleteChosenRatingQualityParams(id);
+        }
+
+        experiment.getAlgorithmQualityRating().getParametersList().forEach(param -> {
+            algorithmsOperations.storeRatingQualityParam(id, param.getId(), param.getValue());
+        });
+
+        if (!Objects.equals(old.getTemplateId(), experimentRecord.getTemplate())) {
+            experimentOperations.deleteRatingOptions(id);
+        }
+
+        if (!experiment.getRatingOptionsList().isEmpty()) {
+            experimentOperations.storeRatingOptions(experiment.getRatingOptionsList(), id);
+        }
+
+        //update the experiment itself
+        experimentOperations.updateExperiment(experimentRecord);
+
+        resulting = fetchExperiment(id);
+        return resulting;
+    }
+
+    /**
+     * updates the state of an experiment
+     * @param id the primary key of the experiment
+     * @param experiment the experiment holding the new data
+     * @param old the old experiment
+     * @return the resulting experiment with the new data
+     */
+    private Experiment updateExperimentState(int id, Experiment experiment, Experiment old) {
+        Experiment resulting;
+
+        //validate the only two possible changes
+        if (!experiment.getState().equals(Experiment.State.PUBLISHED)
+                && !experiment.getState().equals(Experiment.State.CREATIVE_STOPPED))
+            throw new IllegalArgumentException("Only "+ Experiment.State.PUBLISHED.name()+
+                    " and " +Experiment.State.CREATIVE_STOPPED.name()+
+                    " is allowed as state change");
+
+        //validate its draft -> published
+        if (!(experiment.getState().equals(Experiment.State.PUBLISHED) && old.getState().equals(Experiment.State.DRAFT))) {
+            throw new IllegalArgumentException("Publish is only allowed for DRAFT experiments");
+        }
+
+        //validate its published -> creative_stopped
+        if (!(experiment.getState().equals(Experiment.State.CREATIVE_STOPPED) && old.getState().equals(Experiment.State.PUBLISHED))) {
+            throw new IllegalArgumentException("Creative stop is only allowed for published experiments");
+        }
+
+        //check that there are enough datas for publish
+        if (experiment.getState().equals(Experiment.State.PUBLISHED)
+                && experimentOperations.verifyExperimentForPublishing(id)) {
+            throw new IllegalStateException("experiment lacks information needed for publishing");
+        }
+
+        //TODO check if there is a chance to creative stopp is not possible? like no creative answers?
+
+        resulting = fetchExperiment(id);
+        resulting = resulting.toBuilder().setState(experiment.getState()).build();
         return resulting;
     }
 
