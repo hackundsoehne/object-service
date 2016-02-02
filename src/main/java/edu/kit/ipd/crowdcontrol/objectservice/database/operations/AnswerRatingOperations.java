9e81ea415b057ca5ce51a09eac05b112ea850269
Leander Kurscheidt <Leander.Kurscheidt@gmx.de>
2016-02-03 00:03:34 +0100

    database: coded needed methods in AnswerRatingOperations

--- src/main/java/edu/kit/ipd/crowdcontrol/objectservice/database/operations/AnswerRatingOperations.java
+++ src/main/java/edu/kit/ipd/crowdcontrol/objectservice/database/operations/AnswerRatingOperations.java
@@ -11,10 +11,8 @@ import org.jooq.impl.DSL;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 
-import static edu.kit.ipd.crowdcontrol.objectservice.database.model.Tables.ANSWER;
-import static edu.kit.ipd.crowdcontrol.objectservice.database.model.Tables.RATING;
+import static edu.kit.ipd.crowdcontrol.objectservice.database.model.Tables.*;
 
 /**
  * responsible for all queries related to the Answer and Rating Table
@@ -28,14 +26,16 @@ public class AnswerRatingOperations extends AbstractOperations {
     }
 
 
-      /**
+    /**
      * Gets all ratings of a specified answer
      *
      * @param answerRecord answer, whose ratings are requested
      * @return list of ratings of a specified answer
      */
-    public Result<RatingRecord> getRatingsOfAnswer(AnswerRecord answerRecord){
-        return null;
+    public Result<RatingRecord> getRatingsOfAnswer(AnswerRecord answerRecord) {
+        return create.selectFrom(RATING)
+                .where(RATING.ANSWER_R.eq(answerRecord.getIdAnswer()))
+                .fetch();
     }
 
 
@@ -45,45 +45,59 @@ public class AnswerRatingOperations extends AbstractOperations {
      * @param expID specifying the experiment
      * @return list of all answers of a experiment
      */
-    public Result<AnswerRecord> getAnswersOfExperiment(int expID){
-        return null;
+    public Result<AnswerRecord> getAnswersOfExperiment(int expID) {
+        return create.selectFrom(ANSWER)
+                .where(ANSWER.EXPERIMENT.eq(expID))
+                .fetch();
     }
 
 
     /**
      * Fetches all answers of the specified experiment with a quality-value equal or above
      * the given threshold
-     * @param expID of the experiment
+     *
+     * @param expID     of the experiment
      * @param threshold specifying good answers. A good answer has at least a quality-value of given threshold
      * @return Map of workers and a set of matching answerRecords.
      */
-    public Map<WorkerRecord,Set<AnswerRecord>> getGoodAnswersOfExperiment(int expID, int threshold){
-        //TODO
-        return null;
+    public Map<WorkerRecord, List<AnswerRecord>> getGoodAnswersOfExperiment(int expID, int threshold) {
+        return create.select(WORKER.fields())
+                .select(ANSWER.fields())
+                .from(WORKER)
+                .rightJoin(ANSWER).onKey()
+                .where(ANSWER.EXPERIMENT.eq(expID))
+                .and(ANSWER.QUALITY.greaterOrEqual(threshold))
+                .fetchGroups(WORKER, record -> record.into(ANSWER));
     }
 
 
     /**
      * Fetches all ratings of the specified experiment with a quality-value equal or above
      * the given threshold
-     * @param expID of the experiment
+     *
+     * @param expID     of the experiment
      * @param threshold specifying good rating. A good rating has at least a quality-value of given threshold
      * @return Map of workers and a set of matching ratings.
      */
-    public Map<WorkerRecord,Set<RatingRecord>> getGoodRatingsOfExperiment(int expID, int threshold){
-        //TODO
-        return null;
+    public Map<WorkerRecord, List<RatingRecord>> getGoodRatingsOfExperiment(int expID, int threshold) {
+        return create.select(WORKER.fields())
+                .select(RATING.fields())
+                .from(WORKER)
+                .rightJoin(RATING).onKey()
+                .where(ANSWER.EXPERIMENT.eq(expID))
+                .and(ANSWER.QUALITY.greaterOrEqual(threshold))
+                .fetchGroups(WORKER, record -> record.into(RATING));
     }
-
 
 
     /**
      * Returns all ratings of given answer, which have a quality rating above passed threshold
+     *
      * @param answerRecord answer, whose good ratings (specified by given threshold) are returned
-     * @param threshold of type int, which specifies good ratings
+     * @param threshold    of type int, which specifies good ratings
      * @return list of all ratings of given answer with a quality rating equal or greater than given threshold
      */
-    public Result<RatingRecord> getGoodRatingsOfAnswer(AnswerRecord answerRecord, int threshold){
+    public Result<RatingRecord> getGoodRatingsOfAnswer(AnswerRecord answerRecord, int threshold) {
         return null;
     }
 
@@ -93,19 +107,24 @@ public class AnswerRatingOperations extends AbstractOperations {
      *
      * @param map of ratings and matching qualities
      */
-    public void setQualityToRatings(Map<RatingRecord,Integer> map){}
+    public void setQualityToRatings(Map<RatingRecord, Integer> map) {
+
+    }
 
     /**
      * Sets quality rating to an answer
-     * @param answer whose quality is to be set
-     *    @param quality of the answer
+     *
+     * @param answer  whose quality is to be set
+     * @param quality of the answer
      */
-    public void setQualityToAnswer(AnswerRecord answer, int quality){}
+    public void setQualityToAnswer(AnswerRecord answer, int quality) {
 
+    }
 
 
     /**
      * inserts a new answer into the DB
+     *
      * @param answerRecord the record to insert
      * @return the resulting record
      */
@@ -124,14 +143,16 @@ public class AnswerRatingOperations extends AbstractOperations {
     /**
      * Sets the quality-assured-bit for the given answerRecord
      * This indicates, that the answers quality is unlikely to change
+     *
      * @param answerRecord whose quality-assured-bit is set
      */
-    public void setAnswerQualityAssured(AnswerRecord answerRecord){
+    public void setAnswerQualityAssured(AnswerRecord answerRecord) {
         //TODO
     }
 
     /**
      * inserts a new rating into the DB
+     *
      * @param ratingRecord the record to insert
      * @return the resulting record
      */
@@ -148,6 +169,7 @@ public class AnswerRatingOperations extends AbstractOperations {
 
     /**
      * gets the answer with the passed primary key
+     *
      * @param answerID the primary key of the answer
      * @return the answerRecord or emtpy
      */
@@ -171,6 +193,7 @@ public class AnswerRatingOperations extends AbstractOperations {
 
     /**
      * Get a Rating form a id
+     *
      * @param id The id to search for
      * @return a RatingRecord if it exists in the db
      */
@@ -180,6 +203,7 @@ public class AnswerRatingOperations extends AbstractOperations {
 
     /**
      * Returns the list of ratings from a answer
+     *
      * @param answerId the answer which was rated
      * @return A list of ratingRecords
      */
