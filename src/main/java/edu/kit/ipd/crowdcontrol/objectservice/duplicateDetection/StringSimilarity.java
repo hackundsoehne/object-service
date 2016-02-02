2d35deae936515794e7ecf837e408ffa7a2afed5
hectorPascal <luc.k@online.de>
2016-02-02 18:34:25 +0100

    trying shash

--- src/main/java/edu/kit/ipd/crowdcontrol/objectservice/duplicateDetection/StringSimilarity.java
+++ src/main/java/edu/kit/ipd/crowdcontrol/objectservice/duplicateDetection/StringSimilarity.java
@@ -3,5 +3,5 @@ package edu.kit.ipd.crowdcontrol.objectservice.duplicateDetection;
 /**
  * Created by lucaskrauss at 02.02.2016
  */
-public class DuplicateChecker {
+public class StringSimilarity {
 }
