79128734e2020bfe2ce062c2f8fcd050093c5796
Niklas Keller <me@kelunik.com>
2016-02-05 15:24:31 +0100

    Add AnswerRatingTransformer tests and fix the transformer

--- src/main/java/edu/kit/ipd/crowdcontrol/objectservice/database/transformers/AnswerRatingTransformer.java
+++ src/main/java/edu/kit/ipd/crowdcontrol/objectservice/database/transformers/AnswerRatingTransformer.java
@@ -6,20 +6,24 @@ import edu.kit.ipd.crowdcontrol.objectservice.proto.Answer;
 import edu.kit.ipd.crowdcontrol.objectservice.proto.Rating;
 
 import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.List;
 
 /**
  * handles the transformation of the Answers and Ratings from and to the protobuf-definitions
- * @author LeanderK
+ *
+ * @author Leander K.
  * @author Marcel Hollderbach
+ * @author Niklas Keller
  */
 public class AnswerRatingTransformer extends AbstractTransformer {
-
     /**
-     * Convert a record into a protobuf object
-     * @param answerRecord The record of a given answer to convert
-     * @param ratings The list of ratings which should be appended to the Answer
-     * @return The protobuf object with the given data from answerRecord and ratings
+     * Convert a record into a protobuf object.
+     *
+     * @param answerRecord the record of a given answer to convert
+     * @param ratings      the list of ratings which should be appended to the answer
+     *
+     * @return The protobuf object with the given data from answer record and ratings.
      */
     public static Answer toAnswerProto(AnswerRecord answerRecord, List<RatingRecord> ratings) {
         return builder(Answer.newBuilder())
@@ -33,37 +37,41 @@ public class AnswerRatingTransformer extends AbstractTransformer {
                 .addAllRatings(() -> ratings.stream()
                         .map(AnswerRatingTransformer::toRatingProto)
                         .iterator()).build();
-
     }
 
     /**
-     * Converts the given protobuf object into a record
+     * Converts the given protobuf object into a record.
      *
-     * @param answer The protobuf object to convert
+     * @param answer       the protobuf object to convert
      * @param experimentId the experiment of the answer
-     * @return The record with the same data like the answer
+     *
+     * @return The record with the same data like the answer.
      */
     public static AnswerRecord toAnswerRecord(Answer answer, int experimentId) {
-        return merge(new AnswerRecord(), answer, (field, record) -> {
+        AnswerRecord answerRecord = new AnswerRecord();
+        answerRecord.setTimestamp(Timestamp.from(Instant.now()));
+
+        return merge(answerRecord, answer, (field, record) -> {
             switch (field) {
-                case Answer.ID_FIELD_NUMBER: record.setIdAnswer(answer.getId());
+                case Answer.EXPERIMENT_ID_FIELD_NUMBER:
+                    record.setExperiment(experimentId);
                     break;
-                case Answer.EXPERIMENT_ID_FIELD_NUMBER: record.setExperiment(experimentId);
+                case Answer.CONTENT_FIELD_NUMBER:
+                    record.setAnswer(answer.getContent());
                     break;
-                case Answer.CONTENT_FIELD_NUMBER: record.setAnswer(answer.getContent());
-                    break;
-                case Answer.WORKER_FIELD_NUMBER: record.setWorkerId(answer.getWorker());
-                    break;
-                case Answer.QUALITY_FIELD_NUMBER: record.setQuality(answer.getQuality());
+                case Answer.WORKER_FIELD_NUMBER:
+                    record.setWorkerId(answer.getWorker());
                     break;
             }
         });
     }
 
     /**
-     * Converts a Database record into a protobuf Objekt
-     * @param ratingRecord The Record from the database to use
-     * @return the new object created from the Record
+     * Converts a database record into a protobuf object.
+     *
+     * @param ratingRecord the record from the database to use
+     *
+     * @return New object created from the record.
      */
     public static Rating toRatingProto(RatingRecord ratingRecord) {
         return builder(Rating.newBuilder())
@@ -76,29 +84,33 @@ public class AnswerRatingTransformer extends AbstractTransformer {
     }
 
     /**
-     * Converts a rating from a protobuf object into a record
-     * The Quality of a RatingRecord cannot be set and is set to 0
-     * @param rating The informations to use from protobuf
-     * @param answerId the answer which was rated for
+     * Converts a rating from a protobuf object into a record. The quality of a RatingRecord cannot
+     * be set.
+     *
+     * @param rating       the information to use from protobuf
+     * @param answerId     the answer which was rated for
      * @param experimentId the experiment of the answer
+     *
      * @return A RatingRecord
      */
     public static RatingRecord toRatingRecord(Rating rating, int answerId, int experimentId) {
         RatingRecord ratingRecord = new RatingRecord();
         ratingRecord.setAnswerR(answerId);
+        ratingRecord.setTimestamp(Timestamp.from(Instant.now()));
+
         return merge(ratingRecord, rating, (field, record) -> {
             switch (field) {
-                case Rating.EXPERIMENT_ID_FIELD_NUMBER: record.setExperiment(experimentId);
+                case Rating.EXPERIMENT_ID_FIELD_NUMBER:
+                    record.setExperiment(experimentId);
                     break;
-                case Rating.TIME_FIELD_NUMBER: record.setTimestamp(new Timestamp(rating.getTime()));
+                case Rating.RATING_FIELD_NUMBER:
+                    record.setRating(rating.getRating());
                     break;
-                case Rating.RATING_FIELD_NUMBER: record.setRating(rating.getRating());
+                case Rating.FEEDBACK_FIELD_NUMBER:
+                    record.setFeedback(rating.getFeedback());
                     break;
-                case Rating.FEEDBACK_FIELD_NUMBER: record.setFeedback(rating.getFeedback());
-                    break;
-                case Rating.WORKER_FIELD_NUMBER: record.setWorkerId(rating.getWorker());
-                    break;
-                case Rating.QUALITY_FIELD_NUMBER: record.setQuality(rating.getQuality());
+                case Rating.WORKER_FIELD_NUMBER:
+                    record.setWorkerId(rating.getWorker());
                     break;
             }
         });
