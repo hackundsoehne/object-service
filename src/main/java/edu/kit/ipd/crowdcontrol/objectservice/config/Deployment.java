e926f8c7580779fe3502572957c63a518e229821
Niklas Keller <me@kelunik.com>
2016-02-05 21:00:44 +0100

    Add more docs to deployment

--- src/main/java/edu/kit/ipd/crowdcontrol/objectservice/config/Deployment.java
+++ src/main/java/edu/kit/ipd/crowdcontrol/objectservice/config/Deployment.java
@@ -1,5 +1,8 @@
 package edu.kit.ipd.crowdcontrol.objectservice.config;
 
+/**
+ * Deployment variables specific to the control ui.
+ */
 public class Deployment {
     /**
      * Deployment origin of the control ui.
