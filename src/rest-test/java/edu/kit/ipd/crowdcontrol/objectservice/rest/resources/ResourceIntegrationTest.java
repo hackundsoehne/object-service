be3fcda8e395efb3b2e39a7af1f3f89979a41eeb
Niklas Keller <me@kelunik.com>
2016-02-06 17:02:05 +0100

    Combine spark integration tests into one test class

--- src/rest-test/java/edu/kit/ipd/crowdcontrol/objectservice/rest/resources/ResourceIntegrationTest.java
+++ src/rest-test/java/edu/kit/ipd/crowdcontrol/objectservice/rest/resources/ResourceIntegrationTest.java
@@ -6,10 +6,10 @@ import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import edu.kit.ipd.crowdcontrol.objectservice.Main;
 import edu.kit.ipd.crowdcontrol.objectservice.config.ConfigException;
+import edu.kit.ipd.crowdcontrol.objectservice.proto.*;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
+import org.junit.Test;
 import spark.Spark;
 
 import java.io.IOException;
@@ -17,11 +17,8 @@ import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-@RunWith (Suite.class)
-@Suite.SuiteClasses({
-        CalibrationResourceIntegrationTest.class,
-        TemplateResourceIntegrationTest.class
-})
+import static org.junit.Assert.*;
+
 public class ResourceIntegrationTest {
     protected static final String ORIGIN = "http://localhost:4567";
 
@@ -33,6 +30,66 @@ public class ResourceIntegrationTest {
     @AfterClass
     public static void tearDown() {
         Spark.stop();
+    }
+
+    @Test
+    public void calibrations() throws NoSuchMethodException, UnirestException, IllegalAccessException, IOException, InvocationTargetException {
+        CalibrationList list = httpGet("/calibrations", CalibrationList.class);
+        assertSame(0, list.getItemsCount());
+
+        Calibration put = Calibration.newBuilder()
+                .setName("Gender")
+                .setQuestion("What's your gender?")
+                .addAnswers(Calibration.Answer.newBuilder().setAnswer("answer1").build())
+                .addAnswers(Calibration.Answer.newBuilder().setAnswer("answer2").build())
+                .addAcceptedAnswers(Calibration.Answer.newBuilder().setAnswer("answer1").build())
+                .build();
+
+        Calibration calibration = httpPut("/calibrations", put, Calibration.class);
+
+        assertTrue(calibration.getId() > 0);
+        assertEquals(put.toBuilder().setId(0).clearAcceptedAnswers().clearAnswers().build(), calibration.toBuilder().clearAnswers().build());
+
+        Calibration received = httpGet("/calibrations/" + calibration.getId(), Calibration.class);
+        assertEquals(calibration, received);
+
+        list = httpGet("/calibrations", CalibrationList.class);
+        assertSame(1, list.getItemsCount());
+
+        assertNull(httpDelete("/calibrations/" + calibration.getId(), Calibration.class));
+
+        list = httpGet("/calibrations", CalibrationList.class);
+        assertSame(0, list.getItemsCount());
+    }
+
+    @Test
+    public void templates() throws NoSuchMethodException, UnirestException, IllegalAccessException, IOException, InvocationTargetException {
+        TemplateList list = httpGet("/templates", TemplateList.class);
+        assertSame(0, list.getItemsCount());
+
+        Template put = Template.newBuilder()
+                .setName("name")
+                .setContent("content")
+                .setAnswerType(AnswerType.TEXT)
+                .addConstraints(Constraint.newBuilder().setName("constraint").build())
+                .addTags(Tag.newBuilder().setName("tag").build())
+                .build();
+
+        Template template = httpPut("/templates", put, Template.class);
+
+        assertTrue(template.getId() > 0);
+        assertEquals(put, template.toBuilder().setId(0).build());
+
+        Template received = httpGet("/templates/" + template.getId(), Template.class);
+        assertEquals(template, received);
+
+        list = httpGet("/templates", TemplateList.class);
+        assertSame(1, list.getItemsCount());
+
+        assertNull(httpDelete("/templates/" + template.getId(), Template.class));
+
+        list = httpGet("/templates", TemplateList.class);
+        assertSame(0, list.getItemsCount());
     }
 
     public static <T extends Message> T httpGet(String path, Class<T> type) throws UnirestException, NoSuchMethodException, InvocationTargetException, IllegalAccessException, IOException {
