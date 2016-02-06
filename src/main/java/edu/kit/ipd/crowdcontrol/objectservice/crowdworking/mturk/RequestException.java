2eac6136e6e274ec878650148b212eff75d2c4bd
Marcel Hollerbach <marcel-hollerbach@t-online.de>
2016-02-06 13:04:29 +0100

    make it unchecked

--- src/main/java/edu/kit/ipd/crowdcontrol/objectservice/crowdworking/mturk/RequestException.java
+++ src/main/java/edu/kit/ipd/crowdcontrol/objectservice/crowdworking/mturk/RequestException.java
@@ -1,14 +1,16 @@
 package edu.kit.ipd.crowdcontrol.objectservice.crowdworking.mturk;
 
+import org.jooq.lambda.UncheckedException;
+
 /**
  * Thrown when a rest request to mturk fails
  */
-public class RequestException extends Exception {
+public class RequestException extends UncheckedException {
     /**
      * Create a new exception
      * @param s message what exactly failed
      */
     public RequestException(String s) {
-        super(s);
+        super(new Throwable(s));
     }
 }
