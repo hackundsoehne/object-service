5439109f4454a672b19ab5ca766b6dcfe3f1d5a5
Lucas Krauß <luc.k@online.de>
2016-03-06 17:49:05 +0100

    Migrated hash-similarity-functions to corresponding class

--- src/main/java/edu/kit/ipd/crowdcontrol/objectservice/duplicateDetection/Similarity/HashSimilarity.java
+++ src/main/java/edu/kit/ipd/crowdcontrol/objectservice/duplicateDetection/Similarity/HashSimilarity.java
@@ -1,12 +1,7 @@
 package edu.kit.ipd.crowdcontrol.objectservice.duplicateDetection.Similarity;
 
 /**
- * Created by lucaskrauss on 06/03/16.
- * @author lucaskrauss
- *
- *  This class provides the utilities to identify similar hashes. It supports:
- *   - percentual similarity identification
- *   - hamming distance
+ * Created by lckr on 06/03/16.
  */
 public class HashSimilarity {
 
