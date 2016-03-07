4617713a6350caec59df6100b25c263e78ce6def
Lucas Krauß <luc.k@online.de>
2016-03-07 18:34:52 +0100

    DuplicateChecker now fetches images if possible, computes hash for answer based on answer-type. WIP - waiting for response field

--- src/main/java/edu/kit/ipd/crowdcontrol/objectservice/duplicateDetection/DuplicateChecker.java
+++ src/main/java/edu/kit/ipd/crowdcontrol/objectservice/duplicateDetection/DuplicateChecker.java
@@ -1,20 +1,25 @@
 package edu.kit.ipd.crowdcontrol.objectservice.duplicateDetection;
 
 import edu.kit.ipd.crowdcontrol.objectservice.database.model.tables.records.AnswerRecord;
-import edu.kit.ipd.crowdcontrol.objectservice.database.model.tables.records.ExperimentRecord;
 import edu.kit.ipd.crowdcontrol.objectservice.database.operations.AnswerRatingOperations;
+import edu.kit.ipd.crowdcontrol.objectservice.database.operations.ExperimentOperations;
 import edu.kit.ipd.crowdcontrol.objectservice.database.transformers.AnswerRatingTransformer;
 import edu.kit.ipd.crowdcontrol.objectservice.duplicateDetection.Similarity.HashSimilarity;
 import edu.kit.ipd.crowdcontrol.objectservice.duplicateDetection.Similarity.ImageSimilarity;
+import edu.kit.ipd.crowdcontrol.objectservice.duplicateDetection.Similarity.Shingle;
+import edu.kit.ipd.crowdcontrol.objectservice.duplicateDetection.Similarity.StringSimilarity;
 import edu.kit.ipd.crowdcontrol.objectservice.event.EventManager;
-import edu.kit.ipd.crowdcontrol.objectservice.proto.Answer;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.*;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.RunnableFuture;
 
 /**
  * Created by lucaskrauss at 02.02.2016
@@ -23,13 +28,22 @@ import java.util.concurrent.RunnableFuture;
  *         <p>
  *         This class provides the functionallity to find duplicates within all answers of an experiment
  */
-public class DuplicateChecker implements Runnable{
+public class DuplicateChecker implements Runnable {
 
     private final AnswerRatingOperations answerRatingOperations;
+    private final ExperimentOperations experimentOperations;
     private final BlockingQueue<AnswerRecord> queue = new LinkedBlockingQueue<>();
 
-    public DuplicateChecker(AnswerRatingOperations answerRatingOperations) {
+    /**
+     * Constructor
+     *
+     * @param answerRatingOperations
+     */
+    public DuplicateChecker(AnswerRatingOperations answerRatingOperations, ExperimentOperations experimentOperations) {
+
         this.answerRatingOperations = answerRatingOperations;
+        this.experimentOperations = experimentOperations;
+
         EventManager.ANSWER_CREATE.getObservable().subscribe(answerEvent -> {
             try {
                 queue.put(AnswerRatingTransformer.toAnswerRecord(answerEvent.getData(), answerEvent.getData().getExperimentId()));
@@ -37,12 +51,41 @@ public class DuplicateChecker implements Runnable{
                 //TODO react
                 e.printStackTrace();
             }
-            this.run();
+            this.notify();
         });
+
+        this.run();
     }
 
-    public void run(){
+    /**
+     * While running, the DuplicateChecker hashes the answers in the queue and checks them for duplicates.
+     */
+    public void run() {
+        while (true) {
+            AnswerRecord answerRecord = null;
+            try {
+                answerRecord = queue.take();
+            } catch (InterruptedException e) {
+                //TODO react
+                e.printStackTrace();
+            }
+            if (answerRecord != null) {
+                //trying to acquire answer-hash
+                Optional<Long> answerHash = getHashFromAnswer(answerRecord, experimentOperations.getExperiment(answerRecord.getExperiment())
+                        .orElseThrow(() -> new IllegalArgumentException("Error! Can't retrieve the experiment matching to ID!")).getAnswerType());
 
+                if (answerHash.isPresent()) {
+                    Map<AnswerRecord, Long> mapOfHashes = answerRatingOperations.getMapOfHashesOfNonDuplicateAnswers(answerRecord.getExperiment());
+                    mapOfHashes.put(answerRecord, answerHash.get());
+                    checkExperimentForDuplicates(mapOfHashes);
+                } else {//If optional is empty and thus the picture-hashing failed, the answers quality will be set to zero.
+                        // The reason for the failure is described in the response-field
+                    answerRatingOperations.setQualityToAnswer(answerRecord,0);
+                    answerRatingOperations.setAnswerQualityAssured(answerRecord);
+                }
+            }
+
+        }
 
     }
 
@@ -51,29 +94,57 @@ public class DuplicateChecker implements Runnable{
      * If duplicates are found their quality is set to 0 and their quality_assured bit is set.
      * The original answer keeps its quality.
      *
-     * @param experiment the experiment
+     * @param mapOfHashes mapping of answers to their corresponding hashes
      */
-    //Only work for string-answers for now
-    public void checkExperimentForDuplicates(ExperimentRecord experiment) {
-
-
+    public void checkExperimentForDuplicates(Map<AnswerRecord, Long> mapOfHashes) {
         //get duplicates
         Set<AnswerRecord> duplicates = new HashSet<>();
-
-
-        Map<AnswerRecord, Long> mapOfHashes = new HashMap<>(); //TODO replace with db op
         if (mapOfHashes.size() > 1) {
             duplicates.addAll(getDuplicatesFromHash(mapOfHashes));
+            duplicates.forEach(answerRecord -> {
+                answerRatingOperations.setQualityToAnswer(answerRecord, 0);
+                answerRatingOperations.setAnswerQualityAssured(answerRecord);
+            });
         }
-
-        duplicates.forEach(answerRecord -> {
-            answerRatingOperations.setQualityToAnswer(answerRecord, 0);
-            answerRatingOperations.setAnswerQualityAssured(answerRecord);
-        });
     }
 
 
-
+    /**
+     * Retrieves a hash of the given answer based on its type.
+     * If an answer is a string answer, its hash is calculated via the corresponding StringSimilarity method.
+     * Else if an answer is considered a URL this method tries to acquire the corresponding picture from its external source
+     * In the case of failure (e.g. because of a malformed URL or connection issues) the method will return an empty-optional.
+     *
+     * @param answerRecord the answer which should be hashed
+     * @param answerType the type of the answer
+     * @return Optional<Long> if nthe answer could be hashed or an empty-optional otherwise
+     */
+    private Optional<Long> getHashFromAnswer(AnswerRecord answerRecord, String answerType) {
+        if (answerType == null) { //String answer
+            return Optional.of(StringSimilarity.computeSimhashFromShingles(Shingle.getShingle(answerRecord.getAnswer(), 3)));
+        } else { //Picture ulr
+            //PictureFetch
+            BufferedImage image;
+            URL url;
+            try {
+                url = new URL(answerRecord.getAnswer());
+            } catch (MalformedURLException e) {
+                e.printStackTrace();//TODO  specify reason for zero-quality in response field
+                return Optional.empty();
+            }
+            try {
+                image = ImageIO.read(url);
+            } catch (IOException e) {
+                e.printStackTrace(); //TODO  specify reason for zero-quality in response field
+                return Optional.empty();
+            }
+            if (image == null) {
+                //TODO  specify reason for zero-quality in response field
+                return Optional.empty();
+            }
+            return Optional.of(ImageSimilarity.getImageHash(image));
+        }
+    }
 
 
     private Set<AnswerRecord> getDuplicatesFromHash(Map<AnswerRecord, Long> mapOfHashes) {
@@ -81,7 +152,6 @@ public class DuplicateChecker implements Runnable{
 
         //Caparison for small number of answers
         if (mapOfHashes.size() < 100) {
-
             mapOfHashes.forEach((answerRecordA, hashA) -> {
                 mapOfHashes.forEach((answerRecordB, hashB) -> {
                     if (HashSimilarity.getSimilarityFromHash(hashA, hashB) > 0.85 && !answerRecordA.equals(answerRecordB)) {
@@ -92,9 +162,7 @@ public class DuplicateChecker implements Runnable{
 
             //Comparison for larger numbers of answers
         } else {
-
             List<Map.Entry<AnswerRecord, Long>> sortedEntries = new ArrayList<>(mapOfHashes.entrySet());
-
             //Sorts hashes and checks each hash with the next greater one for duplicates.
             //Rotating and sorting hashes again assures that duplicates will be next to each other at some point of time
             for (int i = 0; i < 64; i++) {
@@ -118,7 +186,6 @@ public class DuplicateChecker implements Runnable{
     @Deprecated
     private Set<AnswerRecord> getImageDuplicatesFromSignatures(Map<AnswerRecord, Color[][]> mapOfSignatures) {
         Set<AnswerRecord> duplicates = new HashSet<>();
-
         mapOfSignatures.forEach((answerRecordA, signatureA) -> {
             mapOfSignatures.forEach((answerRecordB, signatureB) -> {
                 if (answerRecordA != answerRecordB) {
