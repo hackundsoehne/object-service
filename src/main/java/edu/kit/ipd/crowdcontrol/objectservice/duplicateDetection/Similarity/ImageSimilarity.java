18fb13b11d421f35c5150381ba32d4a8440e3e13
Lucas Krauß <luc.k@online.de>
2016-03-13 15:31:44 +0100

    Part2 :Implemented two more Image-hashing algorithms

--- src/main/java/edu/kit/ipd/crowdcontrol/objectservice/duplicateDetection/Similarity/ImageSimilarity.java
+++ src/main/java/edu/kit/ipd/crowdcontrol/objectservice/duplicateDetection/Similarity/ImageSimilarity.java
@@ -1,15 +1,11 @@
 package edu.kit.ipd.crowdcontrol.objectservice.duplicateDetection.Similarity;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.FilteredImageSource;
 import java.awt.image.ImageFilter;
 import java.awt.image.ImageProducer;
-import java.io.File;
-import java.io.IOException;
-import java.io.OutputStream;
 
 /**
  * Created by lucaskrauss on 10.02.16.
@@ -32,7 +28,7 @@ public class ImageSimilarity {
 
     final static double MAX_DISTANCE = Math.sqrt(3*(Math.pow(255,2)))*25;
     final static int BASE_SIZE = 300;
-    final static int SAMPLE_SIZE = 60;
+
 
 
     /**
@@ -46,22 +42,22 @@ public class ImageSimilarity {
      */
     public static double identifyImageSimilarityBasedOnSignature(BufferedImage image1, BufferedImage image2) {
 
-        Color[][] signatureImage1 = getImageSignature(image1);
-        Color[][] signatureImage2 = getImageSignature(image2);
+        Color[][] signatureImage1 = getImageSignature(image1,60,5,5);
+        Color[][] signatureImage2 = getImageSignature(image2,60,5,5);
 
         return identifyImageSignatureSimilarity(signatureImage1,signatureImage2);
     }
 
     /**
      * Identifies the similarity of the two given images.
-     * The similarity is based on the hamming-distance of the perceptual-hashes of the two images {@link #getImageHash(BufferedImage)}
+     * The similarity is based on the hamming-distance of the perceptual-hashes of the two images {@link #getImageHashFromGreyScaleDeviation(BufferedImage)}
      * For more details refer to the class-description
      * @param bufferedImage1 first image
      * @param bufferedImage2 second image
      * @return similarity of the two images
      */
     public static double identifyImageSimilarityBasedOnPHash(BufferedImage bufferedImage1, BufferedImage bufferedImage2){
-        return HashSimilarity.getSimilarityFromHash(getImageHash(bufferedImage1),getImageHash(bufferedImage2));
+        return HashSimilarity.getSimilarityFromHash(getImageHashFromGreyScaleDeviation(bufferedImage1), getImageHashFromGreyScaleDeviation(bufferedImage2));
     }
 
     /**
@@ -97,14 +93,15 @@ public class ImageSimilarity {
      * @return a two dimensional Color-array. Each entry Color[x][y] contains the color-value of the image-section
      * (x,y) to (x+1,y+1). The locality of the image-section is preserved.
      */
-    public static Color[][] getImageSignature(BufferedImage image) {
-        if (!(image.getHeight() == BASE_SIZE) && !(image.getWidth() == BASE_SIZE)) {
-            image = rescale(image, BASE_SIZE,BASE_SIZE);
+    public static Color[][] getImageSignature(BufferedImage image, int sampleSize,int signatureHeight, int signatureWidth) {
+        if (!(image.getHeight() == signatureHeight*sampleSize) && !(image.getWidth() == signatureWidth*sampleSize)) {
+            image = rescale(image, signatureHeight*sampleSize,signatureWidth*sampleSize);
         }
-        Color[][] imgSignature = new Color[5][5];
+        Color[][] imgSignature = new Color[signatureWidth][signatureHeight];
+
         for (int imgX = 0; imgX < imgSignature.length; imgX++) {
-            for (int imgY = 0; imgY < imgSignature.length; imgY++) {
-                imgSignature[imgX][imgY] = getAvg(image, imgX, imgY);
+            for (int imgY = 0; imgY < imgSignature[0].length; imgY++) {
+                imgSignature[imgX][imgY] = getAvg(image,sampleSize, imgX, imgY);
             }
         }
         return imgSignature;
@@ -119,20 +116,19 @@ public class ImageSimilarity {
      * @param imgY y-coordinate of the beginning of the image-section
      * @return average color-signature of the image section
      */
-    private static Color getAvg(BufferedImage image, int imgX, int imgY) {
+    private static Color getAvg(BufferedImage image,int sampleSize, int imgX, int imgY) {
         int[] colorBuffer = new int[3];
-        for (int x = imgX * SAMPLE_SIZE; x < (imgX + 1) * SAMPLE_SIZE; x++) {
-            for (int y = imgY * SAMPLE_SIZE; y < (imgY + 1) * SAMPLE_SIZE; y++) {
-                //get color-values for pixel (x,y)
-                Color c = new Color(image.getRGB(x,y));
+        for (int x = imgX * sampleSize; x < (imgX + 1) * sampleSize; x++) {
+            for (int y = imgY * sampleSize; y < (imgY + 1) * sampleSize; y++) {
+                Color c = new Color(image.getRGB(y,x));
                 colorBuffer[0] += c.getRed();
                 colorBuffer[1] += c.getGreen();
                 colorBuffer[2] += c.getBlue();
             }
         }
-        colorBuffer[0] /= SAMPLE_SIZE*SAMPLE_SIZE;
-        colorBuffer[1] /= SAMPLE_SIZE*SAMPLE_SIZE;
-        colorBuffer[2] /= SAMPLE_SIZE*SAMPLE_SIZE;
+        colorBuffer[0] /= sampleSize*sampleSize;
+        colorBuffer[1] /= sampleSize*sampleSize;
+        colorBuffer[2] /= sampleSize*sampleSize;
 
         return new Color(colorBuffer[0], colorBuffer[1], colorBuffer[2]);
     }
@@ -159,12 +155,12 @@ public class ImageSimilarity {
 
     /**
      * Calculates 64-bit perceptual image hash.
-     * The image is rescaled to 8x8-pixles. The hash is based on the grey-value of each pixel in comparision
+     * The image is rescaled to 8x8-pixels. The hash is based on the grey-value of each pixel in comparison
      * to the images overall grey-value.
      * @param bufferedImage source image
      * @return perceptual image hash
      */
-    public static long getImageHash(BufferedImage bufferedImage){
+    public static long getImageHashFromGreyScaleDeviation(BufferedImage bufferedImage){
         bufferedImage = rescale(bufferedImage,8,8);
 
          ImageFilter filter = new GrayFilter(true,50);
@@ -193,4 +189,88 @@ public class ImageSimilarity {
         return hash;
     }
 
+    /**
+     * Calculates a 64-bit hash of the image based on the distribution of the colors and their deviation to the average
+     * color
+     * @param bufferedImage image
+     * @return ash of the image
+     */
+    public static long getImageHashFromColorDeviation(BufferedImage bufferedImage){
+        bufferedImage = rescale(bufferedImage,5,4);
+
+        int averageRed = 0;
+        int averageGreen = 0;
+        int averageBlue = 0;
+        for (int i = 0; i < bufferedImage.getHeight(); i++) {
+            for (int j = 0; j < bufferedImage.getWidth(); j++) {
+                Color c = new Color(bufferedImage.getRGB(j,i));
+                averageRed += c.getRed();
+                averageGreen += c.getGreen();
+                averageBlue += c.getBlue();
+            }
+        }
+        averageRed /=20;
+        averageGreen /=20;
+        averageBlue /=20;
+        long hash = 0;
+
+        for (int i = 0; i < bufferedImage.getHeight(); i++) {
+            for (int j = 0; j < bufferedImage.getWidth(); j++) {
+                Color c = new Color(bufferedImage.getRGB(j,i));
+                hash |= (averageRed > c.getRed()) ? 1 : 0;
+                hash <<= 1;
+                hash |= (averageGreen > c.getGreen()) ? 1 : 0;
+                hash <<= 1;
+                hash |= (averageBlue > c.getBlue()) ? 1 : 0;
+                hash <<= 1;
+            }
+        }
+        hash |= averageRed > averageGreen ? 1 : 0;
+        hash <<= 1;
+        hash |= averageGreen > averageBlue ? 1 : 0;
+        hash <<= 1;
+        hash |= averageBlue > averageRed ? 1 : 0;
+        return hash;
+
+    }
+
+    /**
+     * Calculates 64-bit hash of the image.
+     * The hash-values depends on the locality and the deviation of the colors to the average color.
+     * @param bufferedImage the bufferedImage
+     * @return hashing of the image
+     */
+    public static long getImageHashFromSignature(BufferedImage bufferedImage){
+        Color [][] signature = getImageSignature(bufferedImage,30,4,5);
+        long hash = 0;
+        int avgRed = 0;
+        int avgGreen = 0;
+        int avgBlue = 0;
+        for (Color[] aSignature : signature) {
+            for (Color anASignature : aSignature) {
+                avgRed += anASignature.getRed();
+                avgGreen += anASignature.getGreen();
+                avgBlue += anASignature.getBlue();
+            }
+        }
+        avgRed /=25;
+        avgGreen /= 25;
+        avgBlue /=25;
+        for (int i = 0; i < signature.length-1; i++) {
+            for (int j = 0; j < signature[i].length - 1; j++) {
+                hash |= (signature[i][j].getRed() > avgRed) ? 1 : 0;
+                hash <<= 1;
+                hash |= (signature[i][j].getGreen() > avgGreen) ? 1 : 0;
+                hash <<= 1;
+                hash |= (signature[i][j].getBlue() > avgBlue) ? 1 : 0;
+                        hash <<= 1;
+            }
+        }
+        hash |= avgRed > avgGreen ? 1 : 0;
+        hash <<= 1;
+        hash |= avgGreen > avgBlue ? 1 : 0;
+        hash <<= 1;
+        hash |= avgBlue > avgRed ? 1 : 0;
+        return hash;
+    }
 }
