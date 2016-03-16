c45e77e41f7f7f598ee3b5cf7773178f0a229e5c
Leander Kurscheidt <Leander.Kurscheidt@gmx.de>
2016-03-16 12:02:32 +0100

    db: added storePlatformModes to PopulationsHelper

--- src/main/java/edu/kit/ipd/crowdcontrol/objectservice/database/PopulationsHelper.java
+++ src/main/java/edu/kit/ipd/crowdcontrol/objectservice/database/PopulationsHelper.java
@@ -8,7 +8,9 @@ import edu.kit.ipd.crowdcontrol.objectservice.proto.Calibration;
 import edu.kit.ipd.crowdcontrol.objectservice.proto.Experiment;
 
 import java.util.List;
+import java.util.Map;
 import java.util.function.BiConsumer;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
@@ -18,18 +20,15 @@ import java.util.stream.Collectors;
  * @author LeanderK
  */
 public class PopulationsHelper {
-    private final ExperimentOperations experimentOperations;
     private final CalibrationOperations calibrationOperations;
     private final ExperimentsPlatformOperations experimentsPlatformOperations;
 
     /**
      * Creates a new instance with the given operations to use
-     * @param experimentOperations operations to use
      * @param calibrationOperations operations to use
      * @param experimentsPlatformOperations operations to use
      */
-    public PopulationsHelper(ExperimentOperations experimentOperations, CalibrationOperations calibrationOperations, ExperimentsPlatformOperations experimentsPlatformOperations) {
-        this.experimentOperations = experimentOperations;
+    public PopulationsHelper(CalibrationOperations calibrationOperations, ExperimentsPlatformOperations experimentsPlatformOperations) {
         this.calibrationOperations = calibrationOperations;
         this.experimentsPlatformOperations = experimentsPlatformOperations;
     }
@@ -53,7 +52,7 @@ public class PopulationsHelper {
         List<String> platformsToStore = toStore.stream()
                 .map(Experiment.Population::getPlatformId)
                 .collect(Collectors.toList());
-        experimentOperations.storeExperimentsPlatforms(platformsToStore, experimentId);
+        experimentsPlatformOperations.storeExperimentsPlatforms(platformsToStore, experimentId);
 
         BiConsumer<String, List<Calibration>> storeCalibrations = (platform, calibrations) -> {
             List<Integer> answerIDs = calibrations.stream()
@@ -66,6 +65,21 @@ public class PopulationsHelper {
 
         toStore.forEach(population ->
                 storeCalibrations.accept(population.getPlatformId(), population.getCalibrationsList()));
+
+        Function<Experiment.Population.Task, ExperimentsPlatformModeMode> taskModeMapper = task -> {
+          switch (task) {
+              case BOTH: return ExperimentsPlatformModeMode.normal;
+              case RATING: return ExperimentsPlatformModeMode.rating;
+              case ANSWER: return ExperimentsPlatformModeMode.answer;
+          }
+            return ExperimentsPlatformModeMode.normal;
+        };
+
+        Map<String, ExperimentsPlatformModeMode> platformModes = toStore.stream()
+                .collect(Collectors.toMap(Experiment.Population::getPlatformId,
+                        population -> taskModeMapper.apply(population.getTask())));
+
+        experimentsPlatformOperations.storeExperimentsModes(platformModes, experimentId);
     }
 
     /**
