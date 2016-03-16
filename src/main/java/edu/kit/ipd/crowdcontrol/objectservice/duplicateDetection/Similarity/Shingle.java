155cf4d4d455c8585208e814901d15c20fa2c802
Lucas Krauß <luc.k@online.de>
2016-03-16 19:47:02 +0100

    Migrated duplicate-response to static field. Fixed duplicate-checker test. Added documentation to similarity-classes

--- src/main/java/edu/kit/ipd/crowdcontrol/objectservice/duplicateDetection/Similarity/Shingle.java
+++ src/main/java/edu/kit/ipd/crowdcontrol/objectservice/duplicateDetection/Similarity/Shingle.java
@@ -5,9 +5,20 @@ import java.util.Set;
 
 /**
  * Created by lucaskrauss at 02.02.2016
+ *  @author lucaskrauss
+ *
+ *  Provides the functionality to retreive a set of n-grams of specified size
+ *  of a given string
+ *
  */
 public class Shingle {
 
+    /**
+     * Gets a set of ngrams of the given string
+     * @param input the stringe
+     * @param ngramSize size of the n-grams
+     * @return set of n-grams of the string
+     */
     public static Set<String> getShingle(String input, int ngramSize ){
 
         Set<String> shingles = new HashSet<>();
