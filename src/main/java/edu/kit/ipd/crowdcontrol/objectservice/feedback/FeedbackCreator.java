eeb7aad3825020cac60269aff0acac72b139c34f
Felix Rittler <felix.rittler@web.de>
2016-03-18 00:11:00 +0100

    better layout

--- src/main/java/edu/kit/ipd/crowdcontrol/objectservice/feedback/FeedbackCreator.java
+++ src/main/java/edu/kit/ipd/crowdcontrol/objectservice/feedback/FeedbackCreator.java
@@ -11,10 +11,6 @@ import edu.kit.ipd.crowdcontrol.objectservice.template.Template;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -87,7 +83,7 @@ public class FeedbackCreator {
             answerMap.put("ratings", ratingMessage.toString());
             answerMap.put("systemResponse", systemFeedback);
 
-            answerMessage.append(Template.apply(feedbackAnswer, answerMap));
+            answerMessage.append(Template.apply(feedbackAnswer, answerMap)).append(System.getProperty("line.separator"));
         }
 
         //Send feedback to the last worker
