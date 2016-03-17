eef1075bfe34f86e8ab6e25d75ed0378315be40e
Marcel Hollerbach <marcel-hollerbach@t-online.de>
2016-03-17 23:35:52 +0100

    log it

--- src/main/java/edu/kit/ipd/crowdcontrol/objectservice/Utils.java
+++ src/main/java/edu/kit/ipd/crowdcontrol/objectservice/Utils.java
@@ -3,6 +3,8 @@ package edu.kit.ipd.crowdcontrol.objectservice;
 import com.google.common.base.Charsets;
 import com.google.common.io.CharStreams;
 import edu.kit.ipd.crowdcontrol.objectservice.Main;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -13,6 +15,7 @@ import java.io.InputStreamReader;
  * @author Marcel Hollerbach
  */
 public class Utils {
+    private static final Logger LOGGER = LogManager.getLogger("Utils");
     /**
      * Loads a file located relative to the main class
      * @param file the filename to load
@@ -25,7 +28,7 @@ public class Utils {
                             Main.class.getResourceAsStream(file)
                             , Charsets.UTF_8));
         } catch (IOException e) {
-            e.printStackTrace();
+            LOGGER.fatal("Loading file "+file+" failed", e);
             return "";
         }
     }
