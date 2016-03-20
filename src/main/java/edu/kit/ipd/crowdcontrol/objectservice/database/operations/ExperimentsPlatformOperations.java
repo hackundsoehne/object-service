ab8a843272f72c844a9e7b8262f90153ed3f0249
LeanderK <Leander.Kurscheidt@gmx.de>
2016-03-20 17:01:02 +0100

    db: updated operations to new db version

--- src/main/java/edu/kit/ipd/crowdcontrol/objectservice/database/operations/ExperimentsPlatformOperations.java
+++ src/main/java/edu/kit/ipd/crowdcontrol/objectservice/database/operations/ExperimentsPlatformOperations.java
@@ -1,8 +1,6 @@
 package edu.kit.ipd.crowdcontrol.objectservice.database.operations;
 
-import com.google.common.base.Function;
 import com.google.common.base.Predicate;
-import com.google.common.collect.Sets;
 import edu.kit.ipd.crowdcontrol.objectservice.database.model.enums.ExperimentsPlatformModeMode;
 import edu.kit.ipd.crowdcontrol.objectservice.database.model.enums.ExperimentsPlatformStatusPlatformStatus;
 import edu.kit.ipd.crowdcontrol.objectservice.database.model.tables.ExperimentsPlatformMode;
@@ -14,12 +12,9 @@ import edu.kit.ipd.crowdcontrol.objectservice.database.model.tables.records.Expe
 import org.jooq.DSLContext;
 import org.jooq.Field;
 import org.jooq.Record1;
-import org.jooq.Result;
 import edu.kit.ipd.crowdcontrol.objectservice.proto.Experiment;
-import org.jooq.*;
 import org.jooq.impl.DSL;
 
-import java.sql.Time;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.*;
@@ -238,6 +233,17 @@ public class ExperimentsPlatformOperations extends AbstractOperations {
     }
 
     /**
+     * Searches for a task specified by the experimentsPlatformId.
+     * @param experimentsPlatformId the primary key of the experiments-platform
+     * @return the found task or empty if not found
+     */
+    public Optional<ExperimentsPlatformRecord> getExperimentsPlatform(int experimentsPlatformId) {
+        return create.selectFrom(EXPERIMENTS_PLATFORM)
+                .where(EXPERIMENTS_PLATFORM.IDEXPERIMENTS_PLATFORMS.eq(experimentsPlatformId))
+                .fetchOptional();
+    }
+
+    /**
      * gets all the active platform for the experiment
      * @param experimentId the primary key of the experiment
      * @return a list of platforms
@@ -251,9 +257,9 @@ public class ExperimentsPlatformOperations extends AbstractOperations {
                 .leftOuterJoin(mode2).on(
                         EXPERIMENTS_PLATFORM.IDEXPERIMENTS_PLATFORMS.eq(mode2.EXPERIMENTS_PLATFORM)
                         .and(mode1.TIMESTAMP.lessThan(mode2.TIMESTAMP).or(mode1.TIMESTAMP.eq(mode2.TIMESTAMP)
-                                .and(mode1.IDEXPERIMENTS_PLATFORM_STOPGAP.lessThan(mode2.IDEXPERIMENTS_PLATFORM_STOPGAP))))
+                                .and(mode1.ID_EXPERIMENTS_PLATFORM_MODE.lessThan(mode2.ID_EXPERIMENTS_PLATFORM_MODE))))
                 )
-                .where(mode2.IDEXPERIMENTS_PLATFORM_STOPGAP.isNull())
+                .where(mode2.ID_EXPERIMENTS_PLATFORM_MODE.isNull())
                 .and(EXPERIMENTS_PLATFORM.EXPERIMENT.eq(experimentId))
                 .fetchMap(EXPERIMENTS_PLATFORM.PLATFORM, EXPERIMENTS_PLATFORM_MODE.MODE);
     }
@@ -282,9 +288,9 @@ public class ExperimentsPlatformOperations extends AbstractOperations {
     /**
      * returns the latest statuses for all platforms
      * @param experiment the experiment to get the statuses for
-     * @return the statuses
+     * @return a map where the key are the Experiments-Platform IDs and the value the statuses
      */
-    public Set<ExperimentsPlatformStatusPlatformStatus> getExperimentsPlatformStatusPlatformStatuses (int experiment) {
+    public Map<Integer, ExperimentsPlatformStatusPlatformStatus> getExperimentsPlatformStatusPlatformStatuses (int experiment) {
         ExperimentsPlatformStatus status1 = EXPERIMENTS_PLATFORM_STATUS.as("mode1");
         ExperimentsPlatformStatus status2 = EXPERIMENTS_PLATFORM_STATUS.as("mode2");
         return create.select(EXPERIMENTS_PLATFORM.IDEXPERIMENTS_PLATFORMS, status1.PLATFORM_STATUS)
@@ -293,11 +299,11 @@ public class ExperimentsPlatformOperations extends AbstractOperations {
                 .leftOuterJoin(status2).on(
                         EXPERIMENTS_PLATFORM.IDEXPERIMENTS_PLATFORMS.eq(status2.PLATFORM)
                                 .and(status1.TIMESTAMP.lessThan(status2.TIMESTAMP).or(status1.TIMESTAMP.eq(status2.TIMESTAMP)
-                                        .and(status1.IDEXPERIMENTS_PLATFORM_STATUS.lessThan(status2.IDEXPERIMENTS_PLATFORM_STATUS))))
+                                        .and(status1.ID_EXPERIMENTS_PLATFORM_STATUS.lessThan(status2.ID_EXPERIMENTS_PLATFORM_STATUS))))
                 )
-                .where(status2.IDEXPERIMENTS_PLATFORM_STATUS.isNull())
+                .where(status2.ID_EXPERIMENTS_PLATFORM_STATUS.isNull())
                 .and(EXPERIMENTS_PLATFORM.EXPERIMENT.eq(experiment))
-                .fetchSet(status1.PLATFORM_STATUS);
+                .fetchMap(EXPERIMENTS_PLATFORM.IDEXPERIMENTS_PLATFORMS, status1.PLATFORM_STATUS);
 
     }
 
@@ -349,10 +355,13 @@ public class ExperimentsPlatformOperations extends AbstractOperations {
                 .leftOuterJoin(status2).on(
                         EXPERIMENTS_PLATFORM.IDEXPERIMENTS_PLATFORMS.eq(status2.PLATFORM)
                                 .and(status1.TIMESTAMP.lessThan(status2.TIMESTAMP).or(status1.TIMESTAMP.eq(status2.TIMESTAMP)
-                                        .and(status1.IDEXPERIMENTS_PLATFORM_STATUS.lessThan(status2.IDEXPERIMENTS_PLATFORM_STATUS))))
+                                        .and(status1.ID_EXPERIMENTS_PLATFORM_STATUS.lessThan(status2.ID_EXPERIMENTS_PLATFORM_STATUS))))
                 )
-                .where(status2.IDEXPERIMENTS_PLATFORM_STATUS.isNull())
-                .having(status1.PLATFORM_STATUS.eq(ExperimentsPlatformStatusPlatformStatus.shutdown))
+                .where(status2.ID_EXPERIMENTS_PLATFORM_STATUS.isNull())
+                .having(status1.PLATFORM_STATUS.in(Arrays.asList(
+                        ExperimentsPlatformStatusPlatformStatus.shutdown,
+                        ExperimentsPlatformStatusPlatformStatus.shutdown_failed
+                )))
                 .fetchSet(EXPERIMENTS_PLATFORM.IDEXPERIMENTS_PLATFORMS);
 
         return create.selectFrom(EXPERIMENT)
